1e6664c1a4d13d016fc2c6bdb4efb1c863ab6d04
asbenjamin <shammahbenjamin@gmail.com>
2022-10-26 14:11:34 +0300

    setup for render deploy

--- invoicely/settings.py
+++ invoicely/settings.py
@@ -37,7 +37,8 @@ if RENDER_EXTERNAL_HOSTNAME:
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8081",
     "http://localhost:8080",
-    "http://127.0.0.1:8081"
+    "http://127.0.0.1:8081",
+    "https://accounts-1b5f.onrender.com"
 ]
 
 REST_FRAMEWORK = {
